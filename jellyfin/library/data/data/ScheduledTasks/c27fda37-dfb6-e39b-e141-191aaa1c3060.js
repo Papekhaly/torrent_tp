32ed634d8ae39742d4b40cba2583d6e76be415b0
Papekhaly <papekhaly96@outlook.fr>
2021-02-28 14:49:58 +0100

    volume fixed

--- jellyfin/library/data/data/ScheduledTasks/c27fda37-dfb6-e39b-e141-191aaa1c3060.js
+++ jellyfin/library/data/data/ScheduledTasks/c27fda37-dfb6-e39b-e141-191aaa1c3060.js
@@ -1,1 +1,1 @@
-{"StartTimeUtc":"2021-02-28T12:44:10.6632048Z","EndTimeUtc":"2021-02-28T12:44:10.7222454Z","Status":"Completed","Name":"Update Plugins","Key":"PluginUpdates","Id":"c27fda37dfb6e39be141191aaa1c3060"}
+{"StartTimeUtc":"2021-02-28T13:45:19.8979926Z","EndTimeUtc":"2021-02-28T13:45:20.9655448Z","Status":"Completed","Name":"Update Plugins","Key":"PluginUpdates","Id":"c27fda37dfb6e39be141191aaa1c3060"}
